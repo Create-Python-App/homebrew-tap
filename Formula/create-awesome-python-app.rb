c3bb0c2e7a24c8bfed9367349d5bebf385c18e3a
ulises-jeremias <17727170+ulises-jeremias@users.noreply.github.com>
2026-07-28 14:44:01 +0000

    feat: update formula to 0.2.12

--- Formula/create-awesome-python-app.rb
+++ Formula/create-awesome-python-app.rb
@@ -1,8 +1,8 @@
 class CreateAwesomePythonApp < Formula
   desc "Composable scaffolding CLI for production-ready Python apps"
   homepage "https://github.com/Create-Python-App/create-python-app"
-  url "https://files.pythonhosted.org/packages/f6/a9/45f8e5ec2af158cdaac83f6b48de48cc4c007a95f98b32df7cc3bb31a9d9/create_awesome_python_app-0.2.11.tar.gz"
-  sha256 "98bf74a2f443fdeeea8a25edfcee4f9d8cd0cdf6ca18e090104e9ee8abd00412"
+  url "https://files.pythonhosted.org/packages/43/b3/e3189fc4fffc887b3a77a89742f86b8071ed394110301502e4db457f393b/create_awesome_python_app-0.2.12.tar.gz"
+  sha256 "7cfdec7a537af4637de4647646e4dafe75e41dd269376a8c007bfb2b22262bf1"
   license "MIT"
 
   depends_on "python@3.12"
